97613e7daa45067ef301fb1447acad6764193468
Leonid Vegera <leonid.vegera@gmail.com>
2023-10-09 15:40:22 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const {body} = document;
+
+function scenario(resolve, reject) {
+  logo.addEventListener('click', () => {
+    resolve();
+  })
+
+  setTimeout(() => {
+    reject();
+  }, 3000)
+}
+
+const promise = new Promise(scenario);
+
+promise
+  .then(function() {
+    body.insertAdjacentHTML('beforeend', `
+      <div class="message">Promise was resolved!</div>
+    `)
+  })
+  .catch(() => body.insertAdjacentHTML('beforeend', `
+      <div class="message error-message">Promise was rejected!<div/>
+  `))
